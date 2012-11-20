f5aa5cd7d5e19f98331256e76a7a54091ad26e0d
gnysek <gnysek@localhost>
2012-11-20 15:20:23 +0100

    icons

---
+++ MapEditor/Forms/MapEditorMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MapEditor.Components;
+using MapEditor.Forms;
+
+namespace MapEditor
+{
+	public partial class MapEditorMain : Form
+	{
+		public MapEditorMain()
+		{
+			InitializeComponent();
+		}
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (AboutWindow form = new AboutWindow())
+            {
+                form.ShowDialog();
+            }
+        }
+	}
+}
