ed6879c864670cd22e27c1ba10ba084e46e24982
gnysek <524094+gnysek@users.noreply.github.com>
2016-10-04 22:43:33 +0200

    randomize object placement

---
+++ MapEditor/Common/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Forms;
+
+namespace MapEditor.Common
+{
+    class Helper
+    {
+        public static float getDirFromInput(TextBox t)
+        {
+
+            return parseToFloat(t.Text);
+        }
+
+        public static float parseToFloat(string s)
+        {
+            float a = 0.0f;
+            float.TryParse(s, NumberStyles.Float | NumberStyles.AllowThousands, CultureInfo.InvariantCulture, out a);
+            return a;
+        }
+    }
+}
